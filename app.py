9d910e13a2a8bc7bd2ef9274d608beb817944b49
Saudade08 <carmen.ramospizarro@udc.edu>
2025-05-25 13:05:49 -0700

    Update app.py
    changed to accept whatever port is being used in the env variables

--- app.py
+++ app.py
@@ -240,4 +240,5 @@ def download_logs():
     return send_from_directory(directory=log_directory, path=log_filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
